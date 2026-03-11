69acdbf28a1602045dd92b28473325f225a9fed1
thestvx <kazeeoreki1@gmail.com>
2026-03-11 06:01:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -472,7 +472,7 @@ function updateLangWarning() {
 }
 
 // ─── GOOGLE SHEETS URL ────────────────────────────────────
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxRvZQ2dbtdYJPtNzQHBVQiEpMn-sQPNnSDxMLjnxqzxA7WeC8Ixvr-Viv0UMn5W83uvQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqwCoilqE9X-yqAO8SZ871UjPl7_n-4cuiEeRxJj1pYSHTolxP6-gYETr7b-n-W9FTMQ/exec';
 
 // ─── MODAL ────────────────────────────────────────────────
 let currentModalType = 'support';
